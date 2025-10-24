91ca5bd8620e49171a49165ca656846a17ce92d0
MeDo TaRek <Mohamed_taree2001@yahoo.com>
2025-10-24 14:15:13 +0300

    Update welcome message - remove extra emojis

--- apps/nextjs-app/src/app/page.tsx
+++ apps/nextjs-app/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome 👋 Ya basha 🤣🤣
+              Welcome 👋
             </h1>
           </div>
 
