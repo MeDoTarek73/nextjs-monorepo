9d710ab79c88d27ae045137d9134a8c53479fedb
MeDo TaRek <Mohamed_taree2001@yahoo.com>
2025-10-24 14:10:12 +0300

    Update welcome message to Ya basha

--- apps/nextjs-app/src/app/page.tsx
+++ apps/nextjs-app/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome 👋 Ya basha 🤣🤣🤣
+              Welcome 👋 Ya basha 🤣🤣
             </h1>
           </div>
 
